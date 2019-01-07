27f4df304e9114678216faae233d0a275027d98b
fz3p <fzepparelli@protonmail.com>
2019-01-07 14:49:14 +0100

    send_to_domoticz (temp and hum)

--- README.md
+++ README.md
@@ -1,10 +1,42 @@
-raspberry_0w_chickencoop
+#raspberry_0w_chickencoop
 
-# install Debian 9
+## install Debian 9
 
-# pre-requis 
+## pre-requis 
 `sudo apt-get update && sudo apt-get upgrade`
 `sudo apt-get install python3 RPi.GPIO`
 
 * attribuer les bons droits aux différents fichiers 
 * pour tester : `sudo python3 open_door.py` ou  `sudo python3 close_door.py`
+
+
+# récupération de la température
+
+## installation de python
+`
+git clone https://github.com/adafruit/Adafruit_Python_DHT.git
+cd Adafruit_Python_DHT
+sudo apt-get update
+sudo apt-get install build-essential python-dev python-openssl
+sudo python setup.py install
+`
+
+
+## installation de pip
+`
+sudo apt-get upgrade
+sudo apt-get install python-pip
+sudo pip install requests
+`
+
+
+## rendre executable
+`chmod +x /home/user/script/send_to_domoticz.py`
+
+
+## automatisation 
+`
+sudo crontab -e
+*/1 * * * * sudo /home/user/script/send_to_domoticz.py
+`
+*http://fr.wikipedia.org/wiki/Crontab*# weatherstation
---
+++ send_to_domoticz.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+
+from requests.auth import HTTPBasicAuth
+import requests
+import sys
+import Adafruit_DHT
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# fonction
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+def maj_widget(val_url):
+    requete = 'http://' + domoticz_ip + ':' + domoticz_port + val_url
+    # print requete
+    r = requests.get(requete, auth=HTTPBasicAuth(user, password))
+    if r.status_code != 200:
+        print("Erreur API Domoticz")
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# les parametres de Domoticz
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+domoticz_ip = ''
+domoticz_port = '8080'
+user = ''
+password = ''
+domoticz_idx = ''
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# récuperation des informations des capteurs
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# sensor est 11,22,ou 2302
+# pin est le numero d la pin que vous avez cablée
+# https://pinout.xyz/pinout/pin12_gpio18#
+sensor = 11
+pin = 18
+humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+
+
+if humidity is not None and temperature is not None:
+
+    # https://www.domoticz.com/wiki/Domoticz_API/JSON_URL%27s#Temperature.2Fhumidity.2Fbarometer
+    # modele url : /json.htm?type=command&param=udevice&idx=IDX&nvalue=0&
+    # svalue=TEMP;HUM;
+    # l URL Domoticz pour le widget virtuel
+
+    url = '/json.htm?type=command&param=udevice&idx=' + str(domoticz_idx)
+    url += '&nvalue=0&svalue='
+    url += str('{0:0.1f};{1:0};0').format(temperature, humidity)
+    print(url)
+    maj_widget(url)
+
+else:
+    print('Probleme avec la lecture du DHT11. Try again!')
+    sys.exit(1)
